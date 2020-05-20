c58c5888986b03c9207f684536f2cb4b8e1ef102
brightcat1 <bright11jp@yahoo.co.jp>
2020-05-20 17:26:26 +0900

    データベースへの接続処理を書いた

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 #Flaskとrender_template（HTMLを表示させるための関数）をインポート
 from flask import Flask, render_template, request, url_for, redirect
-
+import mysql.connector as voca_db
 import json
 
 from models.database import MySQL
@@ -8,7 +8,15 @@ from models.database import MySQL
 f = open("loginfo.json", 'r')
 login_info = json.load(f)
 
-db = MySQL(**login_info)
+connect = voca_db.connect(
+    host=login_info["host"],
+    port=login_info["port"],
+    user=login_info["user"],
+    password=login_info["password"],
+    database=login_info["database"]
+)
+connect.ping(reconnect=True)
+cur = connect.cursor()
 
 #Flaskオブジェクトの生成
 app = Flask(__name__)
@@ -28,6 +36,13 @@ def index():
 #「/create_card.html」へアクセスがあった場合に、「create_card.html」を返す
 @app.route("/create_card.html")
 def create_card():
+    return render_template("create_card.html")
+
+@app.route("/add_card",methods=["post"])
+def post():
+    word = request.form["word"]
+    mean = request.form["mean"]
+    cur.execute("INSERT INTO vocabook (word, mean) VALUES ('{word}', '{mean}');".format(word=word, mean=mean))
     return render_template("create_card.html")
 
 #「/test.html」へアクセスがあった場合に、「test.html」を返す
